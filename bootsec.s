e98109f5f0585808cbd1a6a3824e986b7a4edbf6
Gabe Livengood <gabe@ggliv.com>
2023-11-10 22:39:39 -0500

    fix up comments and label names

--- bootsec.s
+++ bootsec.s
@@ -8,14 +8,14 @@
 ; si: source index
 ; di: destination index
 
-[org 0x7c00] ; bios program offset
-[bits 16] ; use 16 bit real mode
+[org 0x7c00]          ; bios program offset
+[bits 16]             ; use 16 bit real mode
 
-mov ax, 0x0003 ; set video mode to 80x25 16-color text
-int 0x10       ; raise video interrupt
+mov ax, 0x0003        ; set video mode to 80x25 16-color text
+int 0x10              ; raise video interrupt
 
 mov dx, 0x0514
-call .draw_board
+call draw_board       ; draw at 4,20
 
 handle_input:
   mov ah, 0x00
@@ -43,7 +43,7 @@ handle_input:
   jmp .echo
 
 
-jmp $ ; infinite loop
+jmp $                 ; infinite loop
 
 ;
 ; Data/functions
@@ -62,16 +62,16 @@ move_left:
   hlt
 
 ; Print the null-terminated string at [si]
-.print_str:
+print_str:
   push si
   .while:
     lodsb
-    or al, al ; are we at a null char?
-    jz .done; ; yes, break
+    or al, al    ; are we at a null char?
+    jz .done     ; yes, break
 
-    mov ah, 0x0e   ; set routine to tty
-    int 0x10   ; raise print interrupt
-    jmp .while ; continue w next char
+    mov ah, 0x0e ; set routine to tty
+    int 0x10     ; raise print interrupt
+    jmp .while   ; continue w next char
 
   .done:
     pop si
@@ -80,77 +80,73 @@ move_left:
 
 ; Put the cursor to the position specified by DX
 ; DH = row, DL = col
-.mov_cur:
-  ; preserve state
-  pusha
+mov_cur:
+  pusha        ; preserve state
   mov ah, 0x02 ; "get cursor data" process
   int 0x10     ; screen function int
-  ; restore state
-  popa
+  popa         ; restore state
   ret
 
 
 ; dh = row
 ; dl = col
-.draw_board:
+draw_board:
   pusha
-  call .mov_cur ; move cursor to correct spot
-  mov si, .top_line ; print top bar
-  call .print_str
-  add dx, 0x0100 ; go to next line
-  call .mov_cur
-  mov si, .int_blank
-  call .print_str ; print internal blank line
+  call mov_cur        ; move cursor to correct spot
+  mov si, top_line    ; print top bar
+  call print_str
+  add dx, 0x0100      ; go to next line
+  call mov_cur
+  mov si, int_blank
+  call print_str      ; print internal blank line
 
-  mov cx, 5 ; loop 5 times
+  mov cx, 5           ; loop 5 times
   .loop:
-    add dx, 0x0100 ; go to next line
-    call .mov_cur
+    add dx, 0x0100    ; go to next line
+    call mov_cur
 
-    mov si, .int_blank
-    call .print_str ; print internal blank line
+    mov si, int_blank
+    call print_str    ; print internal blank line
 
-    add dx, 0x0100 ; go to next line
-    call .mov_cur
+    add dx, 0x0100    ; go to next line
+    call mov_cur
 
-    mov si, .int_border ; print internal border line
-    call .print_str
+    mov si, int_border ; print internal border line
+    call print_str
 
-    add dx, 0x0100 ; go to next line
-    call .mov_cur
+    add dx, 0x0100     ; go to next line
+    call mov_cur
 
-    mov si, .int_blank
-    call .print_str ; print internal blank line
+    mov si, int_blank
+    call print_str     ; print internal blank line
 
     loop .loop
 
-  add dx, 0x0100 ; go to next line
-  call .mov_cur
+  add dx, 0x0100       ; go to next line
+  call mov_cur
 
-  mov si, .int_blank
-  call .print_str ; print internal blank line
+  mov si, int_blank
+  call print_str       ; print internal blank line
 
-  add dx, 0x0100 ; go to next line
-  call .mov_cur
+  add dx, 0x0100       ; go to next line
+  call mov_cur
 
-  mov si, .btm_line ; print top bar
-  call .print_str
+  mov si, btm_line     ; print top bar
+  call print_str
 
   popa
   ret
 
 
-.space:
-  db " ",0
-.top_line:
+top_line:
   db 0xc9,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xbb,0
-.int_blank:
+int_blank:
   db 0xba,0x20,0x20,0x20,0x20,0x20,0xba,0x20,0x20,0x20,0x20,0x20,0xba,0x20,0x20,0x20,0x20,0x20,0xba,0x20,0x20,0x20,0x20,0x20,0xba,0x20,0x20,0x20,0x20,0x20,0xba,0x20,0x20,0x20,0x20,0x20,0xba,0x20,0x20,0x20,0x20,0x20,0xba,0
-.int_border:
+int_border:
   db 0xcc,0xcd,0xcd,0xcd,0xcd,0xcd,0xce,0xcd,0xcd,0xcd,0xcd,0xcd,0xce,0xcd,0xcd,0xcd,0xcd,0xcd,0xce,0xcd,0xcd,0xcd,0xcd,0xcd,0xce,0xcd,0xcd,0xcd,0xcd,0xcd,0xce,0xcd,0xcd,0xcd,0xcd,0xcd,0xce,0xcd,0xcd,0xcd,0xcd,0xcd,0xb9,0
-.btm_line:
+btm_line:
   db 0xc8,0xcd,0xcd,0xcd,0xcd,0xcd,0xca,0xcd,0xcd,0xcd,0xcd,0xcd,0xca,0xcd,0xcd,0xcd,0xcd,0xcd,0xca,0xcd,0xcd,0xcd,0xcd,0xcd,0xca,0xcd,0xcd,0xcd,0xcd,0xcd,0xca,0xcd,0xcd,0xcd,0xcd,0xcd,0xca,0xcd,0xcd,0xcd,0xcd,0xcd,0xbc,0
 
 
 times 510-($-$$) db 0 ; pad with zeros
-dw 0xaa55 ; boot sector magic number
+dw 0xaa55             ; boot sector magic number
