574f3c7e5665948e31c245ccf048a54d429ca532
Gabe Livengood <gabe@ggliv.com>
2023-11-17 15:31:07 -0500

    add cursor movement

--- bootsec.s
+++ bootsec.s
@@ -8,31 +8,35 @@
 ; si: source index
 ; di: destination index
 
+%define board_origin 0x0513
+%define cols 7
+
 [org 0x7c00]           ; bios program offset
 [bits 16]              ; use 16 bit real mode
 
 mov ax, 0x0003         ; set video mode to 80x25 16-color text
 int 0x10               ; raise video interrupt
 
-mov dx, [board_origin]
-call draw_board        ; draw at 5,19
+mov dx, board_origin
+call draw_board        ; draw board
+call move_dropsel
 
 handle_input:
   mov ah, 0x00
   int 0x16             ; wait for keyboard input
 
   cmp ah, 0x39         ; space pressed?
-  jnz left             ; no, check for directions
+  jne left             ; no, check for directions
   call drop_token      ; yes, drop token
   jmp handle_input     ; done, wait for more input
 
   left: cmp ah, 0x4b   ; left arrow pressed?
-  jnz right            ; no, check for right
+  jne right            ; no, check for right
   call move_left       ; yes, move the cursor
   jmp handle_input     ; done, wait for more input
 
   right: cmp ah, 0x4d  ; right arrow pressed?
-  jnz handle_input     ; no, wait for more input
+  jne handle_input     ; no, wait for more input
   call move_right      ; yes, move the cursor
   jmp handle_input     ; done, wait for more input
 
@@ -51,7 +55,6 @@ jmp $                  ; infinite loop
 ; Variables/data
 ;
 
-board_origin dw 0x0513
 col_sel  db 0x00 ; currently selected column
 col0_top db 0x00 ; next row for column 0
 col1_top db 0x00 ; next row for column 1
@@ -61,6 +64,10 @@ col4_top db 0x00 ; next row for column 4
 col5_top db 0x00 ; next row for column 5
 col6_top db 0x00 ; next row for column 6
 
+blank:
+  db " ",0
+cursor_char:
+  db 0x1F,0
 top_line:
   db 0xc9,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xcb,0xcd,0xcd,0xcd,0xcd,0xcd,0xbb,0
 int_blank:
@@ -81,27 +88,59 @@ drop_token:
 
 ; Move the drop cursor left
 move_left:
+  cmp byte [col_sel], 0
+  je .done
+  dec byte [col_sel]
+  call move_dropsel
+.done:
   ret
 
 ; Move the drop cursor right
 move_right:
+  cmp byte [col_sel], cols - 1
+  je .done
+  inc byte [col_sel]
+  call move_dropsel
+.done:
+  ret
+
+; Move the drop select cursor to the value of [col_sel]
+move_dropsel:
+  mov dx, board_origin + 0x0003 - 0x0100
+  mov cx, 0
+.loop:
+  call mov_cur
+  cmp cx, [col_sel]
+  jne .blank
+  mov si, cursor_char
+  call print_str
+  jmp .continue
+.blank:
+  mov si, blank
+  call print_str
+.continue:
+  add dx, 6
+  inc cx
+  cmp cx, cols
+  jne .loop
+
   ret
 
 ; Print the null-terminated string at [si]
 print_str:
   push si
-  .while:
-    lodsb
-    or al, al    ; are we at a null char?
-    jz .done     ; yes, break
+.while:
+  lodsb
+  or al, al    ; are we at a null char?
+  jz .done     ; yes, break
 
-    mov ah, 0x0e ; set routine to tty
-    int 0x10     ; raise print interrupt
-    jmp .while   ; continue w next char
+  mov ah, 0x0e ; set routine to tty
+  int 0x10     ; raise print interrupt
+  jmp .while   ; continue w next char
 
-  .done:
-    pop si
-    ret
+.done:
+  pop si
+  ret
 
 
 ; Put the cursor to the position specified by DX
@@ -117,7 +156,6 @@ mov_cur:
 ; dh = row
 ; dl = col
 draw_board:
-  pusha
   call mov_cur        ; move cursor to correct spot
   mov si, top_line    ; print top bar
   call print_str
@@ -127,26 +165,26 @@ draw_board:
   call print_str      ; print internal blank line
 
   mov cx, 5           ; loop 5 times
-  .loop:
-    add dx, 0x0100    ; go to next line
-    call mov_cur
+.loop:
+  add dx, 0x0100    ; go to next line
+  call mov_cur
 
-    mov si, int_blank
-    call print_str    ; print internal blank line
+  mov si, int_blank
+  call print_str    ; print internal blank line
 
-    add dx, 0x0100    ; go to next line
-    call mov_cur
+  add dx, 0x0100    ; go to next line
+  call mov_cur
 
-    mov si, int_border ; print internal border line
-    call print_str
+  mov si, int_border ; print internal border line
+  call print_str
 
-    add dx, 0x0100     ; go to next line
-    call mov_cur
+  add dx, 0x0100     ; go to next line
+  call mov_cur
 
-    mov si, int_blank
-    call print_str     ; print internal blank line
+  mov si, int_blank
+  call print_str     ; print internal blank line
 
-    loop .loop
+  loop .loop
 
   add dx, 0x0100       ; go to next line
   call mov_cur
@@ -160,7 +198,6 @@ draw_board:
   mov si, btm_line     ; print top bar
   call print_str
 
-  popa
   ret
 
 
